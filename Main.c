4af688b6fb19be9249e0b5f57e1f9837936542b3
Ryan Massey <145584188+RyanMassey2004@users.noreply.github.com>
2023-09-23 10:00:37 -0400

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,59 +1,8 @@
-/*#include <stdio.h>
+#include <stdio.h>
 
-void printWelcomeMenu();
-void printOptions();
-void add();
-void subtract();
+int main()
+{
+	printf("Hello World\n");
 
-
-void main() {
-
-	printWelcomeMenu();
-
-	printOptions();
-
-	int inputNum;
-
-	printf("Enter operation number: ");
-	scanf_s("%1o", &inputNum);
-
-	switch (inputNum)
-	{
-	case 1:
-		add();
-	case 2:
-		subtract();
-	}
-
+	return 0;
 }
-
-void printWelcomeMenu() {
-	printf(" **********************\n");
-	printf("**   Welcome to the   **\n");
-	printf("**   BCS Calculator   **\n");
-	printf(" **********************\n");
-}
-
-void printOptions() {
-	printf("1. Add\n");
-	printf("2. Subtract\n");
-}
-
-void add() {
-	double num1, num2, result;
-	printf("Enter the first value:");
-	scanf_s("%lf", &num1);
-	printf("Enter the second value:");
-	scanf_s("%lf", &num2);
-	result = num1 + num2;
-	printf("%lf + %lf = %lf\n", num1, num2, result);
-}
-
-void subtract() {
-	int num1, num2;
-
-	num1 = 0;
-	num2 = 42;
-
-	int result = num2 / num1;
-} */
